d22bbdb4c288efc17d9001f15a28ed66ab35508c
mateusp7 <mateusdev1337@gmail.com>
2024-07-17 17:11:36 -0300

    feat: string interpolation

--- conceitos-iniciais/kotlin/src/Main.kt
+++ conceitos-iniciais/kotlin/src/Main.kt
@@ -1,7 +1,9 @@
 
 fun main() {
-    var nome = "Mateus"
-    var sobrenome = "Paulo da Silva"
+    val nome = "Mateus"
+    val sobrenome = "Paulo da Silva"
 
     println("Olá, me chamo $nome $sobrenome")
+    println("Double: ${Double.MAX_VALUE}")
+    println("Tamanho da string ${nome.length}")
 }
